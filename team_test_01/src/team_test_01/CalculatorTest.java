4adcf6c4b83b8cc49c4543ebf2329b757cec9c89
박재현 <@DESKTOP-SPKT470>
2019-04-29 10:46:50 +0900

    test

--- team_test_01/src/team_test_01/CalculatorTest.java
+++ team_test_01/src/team_test_01/CalculatorTest.java
@@ -1,31 +1,17 @@
 package team_test_01;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class CalculatorTest {
-	private int nbErrors =0;
+
+	@Test
 	public void testAdd() {
 		Calculator calculator = new Calculator();
-		double result = calculator.add(10, 50);
-		if (result != 60) {
-			throw new IllegalStateException("Bad result: " +result);
+		double result = calculator.add(10,  50);
+		assertEquals(60, result, 0);
 		
-		}
-		
-	}
-	
-	
-	public static void main(String[] args) {
-		CalculatorTest test = new CalculatorTest();
-		try {
-			test.testAdd();
-		}
-		catch (Throwable e) {
-			test.nbErrors++;
-			e.printStackTrace();
-		}
-		if (test.nbErrors > 0) {
-			throw new IllegalStateException("There were" + test.nbErrors + " error(s)");
-			
 		}
 	}
 
-}
